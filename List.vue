8072b0f30031ca5608fcb209bd897d2cb753457c
Tigran Tumasov <shugar348@gmail.com>
2016-12-06 00:26:58 +0300

    init project 🐱

---
+++ List.vue
@@ -0,0 +1,99 @@
+<template lang="pug">
+.header
+  .header-logo
+    | Residents of Mesto Place
+
+  //- .header-login
+  //-   | Log In
+  .header-login
+    .header-name
+      | Nikita Shestakov
+    .header-button
+      | Log out
+
+.users
+  .users-title
+    | Users List
+  .users-item
+    | Tigran Tumasov
+  .users-item
+    | Boris Adimov
+  .users-item
+    | Nikita Golovin
+  .users-item
+    | Alex Taktarov
+  .users-item
+    | Ilya Sayapin
+  .users-item
+    | Anton Undefined
+</template>
+<script type="text/babel">
+  export default {
+    name: "ListComponent",
+
+    data () {
+      return {
+      }
+    },
+
+    methods: {
+    }
+  }
+</script>
+<style lang="scss" scoped rel="stylesheet/scss">
+  .header {
+    background: #eee;
+
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 10px 16px;
+
+    &-login {
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      cursor: pointer;
+    }
+
+    &-button {
+      font-size: 12px;
+      cursor: pointer;
+    }
+
+    &-name {
+      font-weight: bold;
+
+      margin-right: 20px;
+    }
+  }
+
+  .users {
+    margin-top: 50px;
+
+    &-title {
+      padding: 0 16px 20px;
+      font-size: 24px;
+      font-weight: bold;
+    }
+
+    &-item {
+      padding: 10px 16px;
+      cursor: pointer;
+
+      transition: background 0.2s ease, color 0.2s ease;
+      will-change: background, color;
+
+      &:nth-child(odd) {
+        background: #eee;
+      }
+
+      &:hover {
+        background: black;
+        color: #fff;
+      }
+    }
+  }
+</style>
---
+++ Profile.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+.login
+  .login-title
+    | Welcome To Your Personal Area
+  input(type="text" placeholder="Login")
+  input(type="password" placeholder="Password")
+  button(type="submit") Log In
+</template>
+<script type="text/babel">
+  export default {
+    name: "LoginComponent",
+
+    data () {
+      return {
+      }
+    },
+
+    methods: {
+    }
+  }
+</script>
+<style lang="scss" scoped rel="stylesheet/scss">
+  .login {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+
+    &-title {
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+
+    input {
+      margin-top: 15px;
+      width: 100%;
+      padding: 8px 16px;
+      border-radius: 3px;
+      border: 1px #e3e3e3 solid;
+      outline: none;
+
+      transition: border 0.2s ease;
+      will-change: border;
+
+      &:hover {
+         border: 1px #5C6BC0 solid;
+      }
+
+      &:focus {
+        border: 1px #42A5F5 solid;
+      }
+    }
+
+    button {
+      background: #4CAF50;
+      padding: 8px 16px;
+      border-radius: 3px;
+      border: none;
+      color: #fff;
+      margin-top: 15px;
+      text-align: center;
+      cursor: pointer;
+
+      transition: background 0.2s ease;
+      will-change: background;
+
+      &:hover {
+        background: #66BB6A;
+      }
+    }
+  }
+</style>
